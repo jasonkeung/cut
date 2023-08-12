1b3168c955c06e0d16a364a3b24ba8445aa18992
Jason Keung <jason.keungg@gmail.com>
2023-08-11 22:40:51 -0700

    Clean

--- playback.py
+++ playback.py
@@ -1,46 +1,25 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import mixer
+from pytube import YouTube 
 
 SAVE_PATH = "/home/jasonkeung/cut/songs/"
 
 
-
-
-
-
 class Playback:
-
-
-
-
     def play(file):
         mixer.init()
         mixer.music.load(file)
         mixer.music.play()
 
 
-
 class Youtube:
-
-
-
     def download(link):
-        # importing the module 
-        from pytube import YouTube 
-        
-
-        
-        
-        try: 
-            yt = YouTube(link) 
-        except: 
-            print("Connection Error") # to handle exception 
-
-
         stream = YouTube(link).streams.get_audio_only(subtype='mp4')
+
         if not stream:
-            raise Exception("no mp4 audio streams for this video")
+            raise Exception("No mp4 audio streams for this video")
+        
         filename = "_".join(stream.title.lower().split(" ")[0:5]) + "." + stream.default_filename.split(".")[-1]
         stream.download(output_path=SAVE_PATH, filename=filename)
         print(f"Downloaded {filename}")
