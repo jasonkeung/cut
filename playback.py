b51cc2ce90fdcde9d04d78c32ed7586426296174
Jason Keung <jason.keungg@gmail.com>
2023-08-12 00:51:52 -0700

    Add concatenate, playback, and download

--- playback.py
+++ playback.py
@@ -1,35 +1,74 @@
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-from pygame import mixer
+import subprocess
 from pytube import YouTube 
+import moviepy.editor as mp
 
-SAVE_PATH = "/home/jasonkeung/cut/songs/"
+SAVE_PATH = "/Users/jkeung/cut/songs/"
 
+def to_path(filename):
+    return SAVE_PATH + filename + ".wav"
+
+# filename refers to the filename without the path to folder or extension.
 
 class Playback:
-    def play(file):
-        mixer.init()
-        mixer.music.load(file)
-        mixer.music.play()
+    def __init__(self):
+        # Read all files and index what is available
+        self.files = self.read_files()
+
+    def read_files(self):
+        files = []
+        for file in os.listdir(SAVE_PATH):
+            if file.endswith(".wav"):
+                files.append("".join(file.split(".")[:-1]))
+        return files
+    
+    def play(self, filename):
+        if filename not in self.files:
+            print("File not found. Either reinitialize Playback or download the file.")
+            return
+        print(f"Playing {to_path(filename)}")
+        return subprocess.call(["afplay", to_path(filename), "-v", ".2"])
 
 
 class Youtube:
     def download(link):
         stream = YouTube(link).streams.get_audio_only(subtype='mp4')
-
         if not stream:
-            raise Exception("No mp4 audio streams for this video")
-        
-        filename = "_".join(stream.title.lower().split(" ")[0:5]) + "." + stream.default_filename.split(".")[-1]
-        stream.download(output_path=SAVE_PATH, filename=filename)
-        print(f"Downloaded {filename}")
+            raise Exception("No webm audio streams for this video")
+        words = stream.title.lower().split(" ")
+        filename = "_".join(words[0:min(len(words), 3)] + [str(stream.filesize_approx)]) # + stream.default_filename.split(".")[-1]
+        if not os.path.exists(to_path(filename)):
+            stream.download(output_path=SAVE_PATH, filename=filename + ".wav")
+            print(f"Downloaded {filename}")
 
         return filename
 
+class Edit:
 
-link="https://www.youtube.com/watch?v=xWOoBJUqlbI"
-downloaded_filename = Youtube.download(link)
+    # Takes in two filenames, converts to AudioFileClip objects, saves the concatenated CompositeAudioClip, and returns the filename of the concatenated clip
+    def concatenate(filename1, filename2):
+        filename = filename1 + "__" + filename2
+        if not os.path.exists(to_path(filename)):
+            clip1 = mp.AudioFileClip(SAVE_PATH + filename1 + ".wav")
+            clip2 = mp.AudioFileClip(SAVE_PATH + filename2 + ".wav")
+            final_clip = mp.concatenate_audioclips([clip1, clip2])
+            
+            final_clip.write_audiofile(to_path(filename), codec="pcm_s32le")
+
+        return filename
+
+    # def cut(filename, start, end):
+    #     clip = mp.AudioFileClip(SAVE_PATH + filename).subclip(start, end)
+    #     clip.audio.write_audiofile(SAVE_PATH + "cut_" + filename)
+    #     return "cut_" + filename
 
 
+link="https://www.youtube.com/watch?v=E3U_ABHg0J0"
+link2="https://www.youtube.com/watch?v=bbmHk1p4TRk"
+file1 = Youtube.download(link)
+file2 = Youtube.download(link2)
 
-Playback.play(SAVE_PATH + downloaded_filename)
+concatenated = Edit.concatenate(file1, file2)
+
+pb = Playback()
+pb.play(concatenated)
